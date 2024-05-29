129306ae3f0f682125d11a019bc20ba2696334ec
shehininv <shehinsharaf09@gmail.com>
2024-05-29 16:25:14 +0530

    Changed one Spelling mistake

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="hero container">
       <div className="hero-text">
-        <h1>We Esnure better education for a better world</h1>
+        <h1>We Ensure better education for a better world</h1>
         <p>
           Our cutting-edge curriculum is designed to empower students with the
           knowledge, skilla, and experience needed to excel in the dynamic field
